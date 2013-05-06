c818921e94bf55ca640432bfa4212eb584dddeb5
eskerda <eskerda@gmail.com>
2013-05-06 14:50:36 +0200

    Change again the code for tracking
    Seems like google is not eating it

--- header.php
+++ header.php
@@ -28,14 +28,16 @@
         <meta name="viewport" content="width=device-width">
         <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>">
         <script src="<?php echo bloginfo('template_directory');?>/js/vendor/modernizr-2.6.2.min.js"></script>
-        <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        <script type="text/javascript">
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', '<?php echo get_option('google_analytics_id'); ?>']);
+            _gaq.push(['_trackPageview']);
 
-            ga('create', '<?php echo get_option('google_analytics_id'); ?>', '<?php echo $_SERVER['HTTP_HOST']; ?>');
-            ga('send', 'pageview');
+            (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
         </script>
     </head>
     <body>

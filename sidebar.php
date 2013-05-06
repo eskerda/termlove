308e234ba176a1a8526d350f79ad288bb43d0961
eskerda <eskerda@gmail.com>
2013-05-06 13:28:58 +0200

    wrap blog name and descr into a div

--- sidebar.php
+++ sidebar.php
@@ -1,11 +1,13 @@
 <div class="header-container">
     <header id="main">
-        <h2>
-            <a class="custom" href="<?php echo esc_url( home_url( '/' ) ); ?>">
-                <?php bloginfo( 'name' ); ?>
-            </a>
-        </h2>
-        <p><?php bloginfo( 'description' ); ?></p>
+        <div class="description">
+            <h2>
+                <a class="custom" href="<?php echo esc_url( home_url( '/' ) ); ?>">
+                    <?php bloginfo( 'name' ); ?>
+                </a>
+            </h2>
+            <p><?php bloginfo( 'description' ); ?></p>
+        </div>
         <?php
             $args = array(
                 'theme_location'  => 'header-menu',

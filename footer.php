a7fd15917fbf42c8e1bfa799262a11216fcd062b
eskerda <eskerda@gmail.com>
2013-05-15 16:18:11 +0200

    Move and reduce font declaration to the CSS
    Will load at the start (not always good), but now has reduced load.

--- footer.php
+++ footer.php
@@ -1,5 +1,4 @@
         <footer class="main"></footer>
-        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,700italic,400,300,700' rel='stylesheet' type='text/css'>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="<?php echo bloginfo('template_directory');?>/js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
         <script src="<?php echo bloginfo('template_directory');?>/js/plugins.js"></script>

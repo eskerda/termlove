befb0e084b756db9725967ad87d2ff99c8784301
eskerda <eskerda@gmail.com>
2013-05-15 16:17:10 +0200

    Add wordpress head and footer calls for extending themes

--- footer.php
+++ footer.php
@@ -4,5 +4,6 @@
         <script>window.jQuery || document.write('<script src="<?php echo bloginfo('template_directory');?>/js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
         <script src="<?php echo bloginfo('template_directory');?>/js/plugins.js"></script>
         <script src="<?php echo bloginfo('template_directory');?>/js/main.js"></script>
+        <?php wp_footer(); ?>
     </body>
 </html>
--- header.php
+++ header.php
@@ -39,6 +39,7 @@
             var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
             })();
         </script>
+        <?php wp_head(); ?>
     </head>
     <body>
         <!--[if lt IE 7]>

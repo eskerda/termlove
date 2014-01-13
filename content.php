5c9a973be066564c727054c1379abd3be22c542a
eskerda <eskerda@gmail.com>
2014-01-13 08:51:27 +0100

    Small code clean up / whitespace / style

--- content.php
+++ content.php
@@ -1,11 +1,13 @@
+<?php
+
+$title = esc_attr(
+    sprintf(__('Permalink to %s', 'termlove'),the_title_attribute('echo=0'))
+);
+
+?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header>
         <h1>
-            <?php 
-                $title = esc_attr( sprintf( __( 'Permalink to %s', 'termlove' ), 
-                                        the_title_attribute( 'echo=0' ) ) 
-                );
-            ?>
             <a href="<?php the_permalink(); ?>" title="<?php echo $title; ?>" rel="bookmark" class="custom">
                 <?php the_title(); ?>
             </a>
